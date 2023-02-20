d9536bb8b0bb8cea9185158f6f7944b25f180f48
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-02-20 23:16:39 +0100

    Added regex for phone number validation.

--- src/validation/phoneNumberValidation.js
+++ src/validation/phoneNumberValidation.js
@@ -17,30 +17,17 @@ const validatePhoneNumber = (body) => {
             }
         }
     }
-    // 250 monthly requests free 
-    // 1 request per second
-    fetch(`https://phonevalidation.abstractapi.com/v1/?api_key=${process.env.PHONE_VALIDATION_API_KEY}&phone=${phoneNumber}`)
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.valid) {
-                return { success : true }
+    const regex = /(\+|00)(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/;
+    const match = phoneNumber.match(regex);
+    if (!match) {
+        return { 
+            success : false,
+            error : {
+                message : "Invalid phone number."
             }
-            return { 
-                success : false,
-                error : {
-                    data
-                }    
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-            return { 
-                success : false,
-                error : {
-                    message : "Error validating phone number."
-                }
-            }
-        })
+        }
+    }
+    return { success : true }
 }
 
 module.exports = validatePhoneNumber
