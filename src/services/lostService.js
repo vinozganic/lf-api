04aced09c7dff572c89727255d6a721a956d3083
Jakov Vinožganić <yourgitemail@example.com>
2023-02-24 00:21:42 +0100

    The tracking key is now checked every time when saving the new Found/Lost into db.

--- src/services/lostService.js
+++ src/services/lostService.js
@@ -3,10 +3,18 @@ const validate = require("../validation/validation")
 const { generateKey } = require("../helpers/trackingKey")
 
 const addLost = async (body) => {
+    let trackingKey = generateKey()
+    let found = await Lost.findOne({ trackingKey }) // Check if that trackingKey already exists
+    while (found) {
+        trackingKey = generateKey()
+        found = await Lost.findOne({ trackingKey })
+    }
+
     const validationResult = validate(body)
     if (!validationResult.success) {
         return validationResult
     }
+
     const newLost = new Lost({
         trackingKey: generateKey(),
         ...body,
