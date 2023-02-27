84a9db612fa38b0fd9e2412ba4438bc9491dc32e
Fran <fran.sipic@gmail.com>
2023-02-27 19:23:09 +0100

    added test file for phone number validation

---
+++ tests/validation/phoneNumberValidation.test.js
@@ -0,0 +1,36 @@
+const validatePhoneNumber = require("../../src/validation/phoneNumberValidation")
+
+describe("phoneNumberValidation", () => {
+
+    test("returns error when no phone number is provided", () => {
+        const result = validatePhoneNumber({
+            phoneNumber : undefined
+        })
+        expect(result.success).toBe(false)
+        expect(result.error.message).toBe("Missing phone number.")
+    });
+
+    test("returns error when phone number is not a string", () => {
+        const result = validatePhoneNumber({
+            phoneNumber : 123456
+        })
+        expect(result.success).toBe(false)
+        expect(result.error.message).toBe("Invalid phone number. Phone number must be a string.")
+    });
+
+    test("returns error when the phone number does nort match the regex", () => {
+
+        const result = validatePhoneNumber({
+            phoneNumber : "123456"
+        })
+        expect(result.success).toBe(false)
+        expect(result.error.message).toBe("Invalid phone number.")
+    });
+
+    test("returns success when the phone number is valid", () => {
+        const result = validatePhoneNumber({
+            phoneNumber : "+385911125672"
+        })
+        expect(result.success).toBe(true)
+    });
+});
