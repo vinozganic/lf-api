22dba12eb8d98c962147e41411b9282de2c53f55
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-03-01 21:22:43 +0100

    Added client to exports.

--- src/db.js
+++ src/db.js
@@ -62,4 +62,4 @@ const Found = mongoose.model("found", foundSchema)
 const lostSchema = new Schema({}, { strict: false })
 const Lost = mongoose.model("lost", lostSchema)
 
-module.exports = { Found, Lost, connectToMongo, connectToPostgres }
+module.exports = { Found, Lost, connectToMongo, connectToPostgres, client }
