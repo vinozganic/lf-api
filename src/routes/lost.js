7c47bb31f915751785972bfda82baeff0e5980a9
Marko <marko.bolt4@gmail.com>
2023-04-22 01:27:01 +0200

    added sending messages to queue

--- src/routes/lost.js
+++ src/routes/lost.js
@@ -6,37 +6,37 @@ const { sendToQueue } = require("../db")
 const router = express.Router()
 
 router.post("/", async (req, res) => {
-    const data = await addLost(req.body)
-    if (data.success === false) {
-        return res.status(400).json(data)
+    const result = await addLost(req.body)
+    if (result.success === false) {
+        return res.status(400).json(result)
     }
-    const message = generateItemToProcessMessage(result.data, "found")
+    const message = generateItemToProcessMessage(result.data, "lost")
     await sendToQueue(message)
-    res.status(201).json(data)
+    res.status(201).json(result)
 })
 
 router.get("/", async (req, res) => {
-    const data = await getLost()
-    if (data.success === false) {
-        return res.status(400).json(data)
+    const result = await getLost()
+    if (result.success === false) {
+        return res.status(400).json(result)
     }
-    res.status(200).json(data)
+    res.status(200).json(result)
 })
 
 router.post("/batch", async (req, res) => {
-    const data = await getLostBatch(req.body)
-    if (data.success === false) {
-        return res.status(400).json(data)
+    const result = await getLostBatch(req.body)
+    if (result.success === false) {
+        return res.status(400).json(result)
     }
-    res.status(200).json(data)
+    res.status(200).json(result)
 })
 
 router.post("/resolve", async (req, res) => {
-    const data = await resolve(req.body)
-    if (data.success === false) {
-        return res.status(400).json(data)
+    const result = await resolve(req.body)
+    if (result.success === false) {
+        return res.status(400).json(result)
     }
-    res.status(200).json(data)
+    res.status(200).json(result)
 })
 
 module.exports = router
